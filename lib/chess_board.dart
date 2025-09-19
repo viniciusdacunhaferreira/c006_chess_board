efcaa75b960bc11bdcd3a32c6e8c0d0c87f76bce
Vinícius da Cunha Ferreira <viniciusdacunha.ferreira@gmail.com>
2025-09-18 22:34:38 -0300

    Answer 1

--- lib/chess_board.dart
+++ lib/chess_board.dart
@@ -7,7 +7,88 @@ class ChessBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Implement the chess board
-    return const Placeholder();
+    return AspectRatio(
+      aspectRatio: 1,
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: SizedBox(
+          width: 400,
+          height: 400,
+          child: Column(
+            children: [
+              for (int rankIndex = 8; rankIndex >= 1; rankIndex--)
+                _Rank(rankIndex)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Rank extends StatelessWidget {
+  const _Rank(this.index);
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 8 / 1,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          for (int fileIndex = 1; fileIndex <= 8; fileIndex++)
+            _Square(rankIndex: index, fileIndex: fileIndex)
+        ],
+      ),
+    );
+  }
+}
+
+class _Square extends StatelessWidget {
+  const _Square({required this.rankIndex, required this.fileIndex});
+
+  final int rankIndex;
+  final int fileIndex;
+
+  bool _isDark() => rankIndex.isOdd == fileIndex.isOdd;
+
+  @override
+  Widget build(BuildContext context) {
+    final resolvedTextStyle = TextStyle(
+      color: _isDark() ? ChessColors.lightSquare : ChessColors.darkSquare,
+      fontWeight: FontWeight.bold,
+      fontSize: 16,
+    );
+
+    return AspectRatio(
+      aspectRatio: 1 / 1,
+      child: Container(
+        padding: EdgeInsets.all(4),
+        color: _isDark() ? ChessColors.darkSquare : ChessColors.lightSquare,
+        child: Stack(
+          children: [
+            Offstage(
+              offstage: fileIndex != 1,
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Text('$rankIndex', style: resolvedTextStyle),
+              ),
+            ),
+            Offstage(
+              offstage: rankIndex != 1,
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  String.fromCharCode(96 + fileIndex),
+                  style: resolvedTextStyle,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
